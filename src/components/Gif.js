29bbf8e1e5f2f834b224d978d70f22a7fde1f99f
mattfrancis888 <mattfrancis888@gmail.com>
2019-05-23 23:16:58 -0400

    Body layout is responsive for all screens

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Gif = (props) => {
   return (
-    <li className="col-12 my-1">
-      <img src={props.url} className="w-100" alt=""/>
+    <li className="gif-wrap col-12 col-md-6 col-lg-4 border-dark">
+      <img src={props.url} className="h-100 w-100" alt=""/>
     </li>
   );
 }
