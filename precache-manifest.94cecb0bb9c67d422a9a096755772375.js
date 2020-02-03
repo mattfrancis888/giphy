815267e55020bdfc44cfbcb06d201ccf48f000f7
Matthew Francis <mattfrancis888@gmail.com>
2020-02-02 23:21:48 -0500

    Updates

---
+++ precache-manifest.94cecb0bb9c67d422a9a096755772375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b9a44ea395f89ef510c1365b1ca7ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "57b97c1d5326ad3e51c1",
+    "url": "./static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "8a0131b49670c35d8e74",
+    "url": "./static/css/main.6828eec1.chunk.css"
+  },
+  {
+    "revision": "57b97c1d5326ad3e51c1",
+    "url": "./static/js/2.1f2462f0.chunk.js"
+  },
+  {
+    "revision": "8a0131b49670c35d8e74",
+    "url": "./static/js/main.b397b9cf.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
